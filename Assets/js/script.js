6b5ab7c9a74bf0d25ca52ab700ba0df2810bce2c
Preksha Gandhi <prekshagandhi@gmail.com>
2021-09-24 21:26:00 -0400

    Updated saving textarea and update ReadMe

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,5 +15,24 @@ $(".container").children("div.time-block").each(function() {
     if (divTime.isBefore(nowTime, 'hour')) {
         $(this).next().addClass('past');
     }
-  });
+});
 
+$(".container").children("button").click(function(event) {
+    event.preventDefault();
+    console.log("event trigger");
+    var key = $(this).prev().attr('id');
+    var textArea = $(this).prev().val();
+    console.log(key);
+    console.log(textArea);
+    localStorage.setItem(key,textArea);
+});
+
+$('#Text1').val(localStorage.getItem('Text1'));
+$('#Text2').val(localStorage.getItem('Text2'));
+$('#Text3').val(localStorage.getItem('Text3'));
+$('#Text4').val(localStorage.getItem('Text4'));
+$('#Text5').val(localStorage.getItem('Text5'));
+$('#Text6').val(localStorage.getItem('Text6'));
+$('#Text7').val(localStorage.getItem('Text7'));
+$('#Text8').val(localStorage.getItem('Text8'));
+$('#Text9').val(localStorage.getItem('Text9'));
